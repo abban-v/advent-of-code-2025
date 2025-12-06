9dc68120a29973750f05f1bc8906e6bd0918df33
Abban C Varughese <abban.varughese@gmail.com>
2025-12-06 17:04:07 +0000

    Day 4 part 2

---
+++ Day4/day4-part2.py
@@ -0,0 +1,29 @@
+file=open("input.txt")
+layout=[]
+movablerolls=0
+for i in file.read().split('\n'):
+    layout.append(list(i))
+while True:
+    movable=False
+    for i in range(len(layout)):
+        for j in range(len(layout[i])):
+            if layout[i][j]!="@":
+                continue
+            adjacentroll=0
+            for k in range(i-1,i+2):
+                if k<0 or k>len(layout)-1:
+                    continue
+                for l in range(j-1,j+2):
+                    if l<0 or l>len(layout[i])-1:
+                        continue
+                    if k==i and l==j:
+                        continue
+                    if layout[k][l]=='@':
+                        adjacentroll+=1
+            if adjacentroll<4:
+                movable=True
+                layout[i][j]='.'
+                movablerolls+=1
+    if movable==False:
+        break
+print(movablerolls)
